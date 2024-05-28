ef482a139d210d2f7acc964015fd351365c9282e
Aziza0011 <khakimovaaziza95@gmail.com>
2024-05-28 09:47:15 +0500

    First commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const username = prompt("Enter here your name");
+const lastname = prompt("Enter here your lastname");
+const address = prompt("Enter here your address");
+const hobby = prompt("Enter here your hobby");
+console.log(username);
+console.log(lastname);
+console.log(address);
+console.log(hobby);
